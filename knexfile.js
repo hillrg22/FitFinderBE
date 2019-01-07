0a7c188459ba914df5905e5d652b033c123d9077
hillrg22 <hillrg22@gmail.com>
2019-01-07 12:50:06 -0700

    installed knex and pg dependencies, created connection file and updated knexfile

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/fitfinder'
+  },
+
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?SSL=true'
+
+};
