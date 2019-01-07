573895b9ad660ca1ccc10308e70e4ad5693d0cfd
hillrg22 <hillrg22@gmail.com>
2019-01-07 12:15:45 -0700

    installed body-parser and cors, basic get route working

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const express = require('express')
 const app = express()
 const port =  process.env.PORT || 3000
 
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+
+
+
+app.get('/', (req,res,next) =>{
+  res.send("🦇🦇🦇🦇")
+})
+
+
+
+
+
+
+
+app.use(notFound)
+
+app.use(errorHandler)
+
+
+function notFound(req, res, next) {
+  res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl })
+}
+
+
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+}
+
 
 
 app.listen(port, () =>{
