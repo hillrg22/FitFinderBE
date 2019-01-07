062a1cdd2fc2160b6772e2a5338b8d30a8d441e5
hillrg22 <hillrg22@gmail.com>
2019-01-07 13:55:17 -0700

    renamed clothing_article migration because of reference error

--- migrations/20190107130009_retailer_clothing_article.js
+++ migrations/20190107130009_retailer_clothing_article.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('clothing_type', function (table){
+  return knex.schema.createTable('retailer_clothing_article', function (table){
   table.increments('id')
   table.integer('clothing_article_id').references('clothing_article.id').unsigned().onDelete('cascade')
   table.integer('retailer_id').references('retailer.id').unsigned().onDelete('cascade')
