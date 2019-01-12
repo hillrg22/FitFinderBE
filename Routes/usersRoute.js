b6ea36dcd3d1d0070a631847ccd9e3a0904cd9e7
hillrg22 <hillrg22@gmail.com>
2019-01-12 11:22:34 -0700

    adding to users route

--- Routes/usersRoute.js
+++ Routes/usersRoute.js
@@ -11,6 +11,55 @@ router.get('/', (req,res,next) => {
 })
 
 
+router.get('/:id', (req,res,next) =>{
+const id = req.params.id
+  knex('users')
+  .where('id',id)
+  .then((user) =>{
+    if(!user.length){
+      next()
+    }
+    else{
+    res.json({user: user[0]})
+    }
+  })
+})
+
+router.post('/', (req,res,next) =>{
+  // const body = req.body
+  knex('student')
+    .insert(req.body)
+    .returning('*')
+    .then((student) =>{
+      console.log(student)
+      res.json({student: student})
+    })
+})
+router.put('/:id', (req,res,next) => {
+  const id = req.params.id
+  const body = req.body
+
+  knex('student')
+    .where('id', id)
+    .update(body)
+    .returning('*')
+    .then(updatedStudent =>{
+      res.json({beer: updatedStudent[0]})
+    })
+})
+
+router.delete('/:id', (req,res,next) => {
+  const id = req.params.id
+  knex('student')
+  .where('id', id)
+  .del()
+  .returning('*')
+  .then(deletedStudent => {
+    res.json({student:deletedStudent[0]})
+  })
+})
+
+
 
 
 module.exports = router
